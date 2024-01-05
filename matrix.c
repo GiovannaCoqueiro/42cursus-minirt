a01edeec7300d52c1423fc7ce9f88ac94d3c4055
Giovanna Coqueiro <gcoqueir@student.42sp.org.br>
2024-01-05 12:25:46 -0300

    matrix

--- matrix.c
+++ matrix.c
@@ -1,149 +1,280 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include <X11/keysym.h>
 #include <X11/X.h>
+#include <X11/keysym.h>
+#include <math.h>
 #include <mlx.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-double **create_matrix(double size)
+typedef struct s_coordinates
 {
-    double **matrix;
-    int     i;
+	double x;
+	double y;
+	double z;
+	double w;
+}	t_coordinates;
 
-    matrix = calloc(size + 1, sizeof(double *));
-    i = -1;
-    while (++i < size)
-    {
-        matrix[i] = calloc(size, sizeof(double));
-    }
-    return (matrix);
+int	matrix_length(double **matrix)
+{
+	int	i;
+
+	i = 0;
+	while (matrix[i])
+		i++;
+	return (i);
 }
 
-int equal_matrixes(double **a, int asize, double **b, int bsize)
+double	**create_matrix(double size)
 {
-    int i;
-    int j;
+	double	**matrix;
+	int		i;
 
-    if (asize != bsize)
-        return (0);
-    i = -1;
-    while (++i < asize)
-    {
-        j = -1;
-        while (++j < asize)
-        {
-            if (a[i][j] != b[i][j])
-                return (0);
-        }
-    }
-    return (1);
+	matrix = calloc(size + 1, sizeof(double *));
+	i = -1;
+	while (++i < size)
+	{
+		matrix[i] = calloc(size, sizeof(double));
+	}
+	return (matrix);
 }
 
-double calculate_multiply_matrix(double **a, double **b, int row, int col)
+int	equal_matrixes(double **a, int asize, double **b, int bsize)
 {
-    double result;
-    int i;
+	int	i;
+	int	j;
 
-    result = 0;
-    i = -1;
-    while (++i < 4)
-    {
-        result += (a[row][i] * b[i][col]);
-    }
-    return (result);
+	if (asize != bsize)
+		return (0);
+	i = -1;
+	while (++i < asize)
+	{
+		j = -1;
+		while (++j < asize)
+		{
+			if (a[i][j] != b[i][j])
+				return (0);
+		}
+	}
+	return (1);
 }
 
-double **multiply_matrix(double **a, double **b)
+double	calculate_multiply_matrix(double **a, double **b, int row, int col)
 {
-    int i;
-    int j;
-    double **c;
+	double	result;
+	int		i;
 
-    c = create_matrix(4);
-    i = -1;
-    while (++i < 4)
-    {
-        j = -1;
-        while (++j < 4)
-            c[i][j] = calculate_multiply_matrix(a, b, i, j);
-    }
-    return (c);
+	result = 0;
+	i = -1;
+	while (++i < 4)
+	{
+		result += (a[row][i] * b[i][col]);
+	}
+	return (result);
 }
 
-double calculate_multiply_matrix_tuple(double **a, double *tuple, int row)
+double	**multiply_matrix(double **a, double **b)
 {
-    double result;
-    int i;
+	int		i;
+	int		j;
+	double	**c;
 
-    result = 0;
-    i = -1;
-    while (++i < 4)
-    {
-        result += (a[row][i] * tuple[i]);
-    }
-    return (result);
+	c = create_matrix(4);
+	i = -1;
+	while (++i < 4)
+	{
+		j = -1;
+		while (++j < 4)
+			c[i][j] = calculate_multiply_matrix(a, b, i, j);
+	}
+	return (c);
 }
 
-double *multiply_matrix_tuple(double **a, double *tuple)
+double	calculate_multiply_matrix_tuple(double **a, double *tuple, int row)
 {
-    int i;
-    int j;
-    double *c;
+	double	result;
+	int		i;
 
-    c = calloc(sizeof(double), 4);
-    i = -1;
-    while (++i < 4)
-    {
-        c[i] = calculate_multiply_matrix_tuple(a, tuple, i);
-    }
-    return (c);
+	result = 0;
+	i = -1;
+	while (++i < 4)
+	{
+		result += (a[row][i] * tuple[i]);
+	}
+	return (result);
 }
 
-int main(void)
+t_coordinates multiply_matrix_tuple(double **a, t_coordinates b)
 {
-    double **a = create_matrix(4);
-    double **b = create_matrix(4);
-    a[0][0] = 1;
-    a[0][1] = 2;
-    a[0][2] = 3;
-    a[0][3] = 4;
-    a[1][0] = 2;
-    a[1][1] = 4;
-    a[1][2] = 4;
-    a[1][3] = 2;
-    a[2][0] = 8;
-    a[2][1] = 6;
-    a[2][2] = 4;
-    a[2][3] = 1;
-    a[3][0] = 0;
-    a[3][1] = 0;
-    a[3][2] = 0;
-    a[3][3] = 1;
- 
-    b[0][0] = -2;
-    b[0][1] = 1;
-    b[0][2] = 2;
-    b[0][3] = 3;
-    b[1][0] = 3;
-    b[1][1] = 2;
-    b[1][2] = 1;
-    b[1][3] = -1;
-    b[2][0] = 4;
-    b[2][1] = 3;
-    b[2][2] = 6;
-    b[2][3] = 5;
-    b[3][0] = 1;
-    b[3][1] = 2;
-    b[3][2] = 7;
-    b[3][3] = 8;
-    double d[4] = {1, 2, 3, 1};
-    double *c = multiply_matrix_tuple(a, d);
-    int i = -1;
-    while (++i < 4)
-    {
-        printf("%f ", c[i]);
-        if (i == 3)
-            printf("\n");
-    }
-    printf("%d\n", equal_matrixes(a, 4, b, 4));
+	t_coordinates	tuple;
+
+	tuple.x = a[0][0] * b.x + a[0][1] * b.y + a[0][2] * b.z + a[0][3] * b.w;
+	tuple.y = a[1][0] * b.x + a[1][1] * b.y + a[1][2] * b.z + a[1][3] * b.w;
+	tuple.z = a[2][0] * b.x + a[2][1] * b.y + a[2][2] * b.z + a[2][3] * b.w;
+	tuple.w = a[3][0] * b.x + a[3][1] * b.y + a[3][2] * b.z + a[3][3] * b.w;
+	return (tuple);
+}
+
+double	**creating_identity_matrix(int size)
+{
+	int		i;
+	double	**matrix;
+
+	matrix = create_matrix(size);
+	i = 0;
+	while (i < size)
+	{
+		matrix[i][i] = 1;
+		i++;
+	}
+	return (matrix);
+}
+
+double	**transposing_matrix(double **matrix)
+{
+	int		size;
+	int		i;
+	int		j;
+	double	**trans_matrix;
+
+	size = matrix_length(matrix);
+	trans_matrix = create_matrix(size);
+	while (i < size)
+	{
+		j = 0;
+		while (j < size)
+		{
+			trans_matrix[j][i] = matrix[i][j];
+			j++;
+		}
+		i++;
+	}
+	return (trans_matrix);
+}
+
+// double determinant_2x2(double **matrix)
+// {
+// 	double determinant;
+
+// 	determinant = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
+// 	return (determinant);
+// }
+
+double **calculate_submatrix(double **matrix, int row, int col)
+{
+	double **submatrix;
+	int i;
+	int j;
+	int k;
+	int l;
+
+	submatrix = create_matrix(matrix_length(matrix) - 1);
+	i = -1;
+	k = 0;
+	while (++i < matrix_length(matrix))
+	{
+		if (i != row)
+		{
+			j = -1;
+			l = 0;
+			while (++j < matrix_length(matrix))
+			{
+				if (j != col)
+				{
+					submatrix[k][l] = matrix[i][j];
+					l++;
+				}
+			}
+			k++;
+		}
+	}
+	return (submatrix);
+}
+
+// double	minor(double **matrix, int row, int col)
+// {
+// 	double **submatrix;
+// 	double determinant;
+
+// 	submatrix = calculate_submatrix(matrix, row, col);
+// 	determinant = determinant_2x2(submatrix);
+// 	free(submatrix);
+// 	return (determinant);
+// }
+
+// double cofactor(double **matrix, int row, int col)
+// {
+// 	double determinant;
+
+// 	determinant = minor(matrix, row, col);
+// 	if ((row + col) % 2  != 0)
+// 		determinant *= -1;
+// 	return (determinant);
+// }
+// double determinant_3x3(double **matrix)
+// {
+// 	double determinant;
+// 	int col;
+
+// 	determinant = 0;
+
+// 	col = -1;
+// 	while (++col < matrix_length(matrix))
+// 		determinant += matrix[0][col] * cofactor(matrix, 0, col);
+// 	return (determinant);
+// }
+
+double determinant_4x4(double **matrix)
+{
+	double determinant;
+	int col;
+	double **submatrix;
+	double minor;
+
+	determinant = 0;
+	if (matrix_length(matrix) == 1)
+		return (matrix[0][0]);
+	else
+	{
+		col = -1;
+		while (++col < matrix_length(matrix))
+		{
+			submatrix = calculate_submatrix(matrix, 0, col);
+			minor = determinant_4x4(submatrix);
+			if (col % 2)
+				determinant -= matrix[0][col] * minor;
+			else
+				determinant += matrix[0][col] * minor;
+			free(submatrix);
+		}
+	}
+	return (determinant);
+}
+
+int	main(void)
+{
+	double	**a;
+	double	**b;
+	double	d[4];
+	double	*c;
+	int		i;
+
+	a = create_matrix(4);
+	// b = create_matrix(4);
+	a[0][0] = -2;
+	a[0][1] = -8;
+	a[0][2] = 3;
+	a[0][3] = 5;
+	a[1][0] = -3;
+	a[1][1] = 1;
+	a[1][2] = 7;
+	a[1][3] = 3;
+	a[2][0] = 1;
+	a[2][1] = 2;
+	a[2][2] = -9;
+	a[2][3] = 6;
+	a[3][0] = -6;
+	a[3][1] = 7;
+	a[3][2] = 7;
+	a[3][3] = -9;
+	printf("%f\n", determinant_4x4(a));
+	// printf("%f\n", determinant_2x2(a));
 }
